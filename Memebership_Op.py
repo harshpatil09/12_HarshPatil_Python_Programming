f0f94daf5164e3c70f5ae36e091f50c1968be2f8
harshpatil09 <139743913+harshpatil09@users.noreply.github.com>
2023-07-31 00:23:01 -0700

    Add files via upload
    Operators in Python

---
+++ Memebership_Op.py
@@ -0,0 +1,5 @@
+#Memebership Operators
+
+l=[2,3,4,5,6,7,8]
+print("Is 6 is Present in ",l,": ",6 in l)
+print("Is 7 is not Present in ",l,": ",7 not in l)
---
+++ comparison_op.py
@@ -0,0 +1,18 @@
+#Comparison Operators
+
+import operator as op
+a=10;b=20
+print("a & b are : ",a,b)
+print()
+print("a>=b: ",op.ge(a,b))
+print()
+print("a<=b: ",op.le(a,b))
+print()
+print("a==b: ",op.eq(a,b))
+print()
+print("a!=b: ",op.ne(a,b))
+print()
+print("a is b: ",a is b)
+print()
+print("a is not b: ",a is not b)
+print()
