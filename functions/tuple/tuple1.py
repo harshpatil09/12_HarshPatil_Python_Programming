a09a3f633a9d6c1d10a12c1f7d091ee4ee80d775
harshpatil09 <139743913+harshpatil09@users.noreply.github.com>
2023-08-14 00:50:02 -0700

    Add files via upload

---
+++ functions/tuple/tuple1.py
@@ -0,0 +1,13 @@
+t1=(1,2,3,4,2)
+print("Tuple : ",t1)
+t1=t1*2
+print("t1=t1*2 : ",t1)
+
+c=t1.count(2)
+print("total no. of 2 : ",c)
+
+i=t1.index(4)
+print("Index of 4 : ",i)
+
+del(t1)
+print(t1)
